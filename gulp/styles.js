9c00a2472d68860823aac032f134574cd1e3f78f
Mark Hoffmeyer <mhoffmeyer@gmail.com>
2015-05-21 15:54:15 -0400

    Removing external gulp and bower dependencies. Getting rid of ruby-sass.

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,10 +2,10 @@ module.exports = function(environment) {
   'use strict';
   var gulp = require('gulp');
   var run = require('run-sequence');
-  var sass = require('gulp-ruby-sass');
+  var sass = require('gulp-sass');
   var minifycss = require('gulp-minify-css');
   var rename = require('gulp-rename');
-  var scsslint = require('gulp-scss-lint');
+ // var scsslint = require('gulp-scss-lint');
   var rimraf = require('rimraf');
 
 
@@ -23,11 +23,11 @@ module.exports = function(environment) {
       .pipe(scsslint());
   });
 
-  gulp.task('sass', function() {
-    return sass('./core/scss/core.scss')
-      .pipe(gulp.dest('./target/css/'));
+  gulp.task('sass', function () {
+    gulp.src('./core/scss/core.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./target/css/'));
   });
-
 
   gulp.task('minify-css', function() {
     return gulp.src(['./target/css/*.css'])
@@ -40,7 +40,7 @@ module.exports = function(environment) {
 
   gulp.task('styles', function() {
     return run('clean-styles',
-      'scss-lint',
+    //  'scss-lint',
       'sass',
       'minify-css');
   });
